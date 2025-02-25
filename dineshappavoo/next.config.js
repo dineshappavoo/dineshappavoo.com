98d9714c460e36ee54d71e3b78a917bb3e214eea
Dinesh Appavoo <dinesh@howti.io>
2025-02-24 19:13:01 -0500

    Updated the local files to move to js instead of ts

---
+++ dineshappavoo/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // ✅ Enables styled-components support
+  },
+};
+
+module.exports = nextConfig;
