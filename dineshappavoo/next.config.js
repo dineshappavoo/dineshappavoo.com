9e2b7a8176cb67fee164f0c18502ad9919832d2d
Dinesh Appavoo <dinesha.cit@gmail.com>
2025-12-06 11:25:06 -0800

    Updated next.config.js with a simple version

--- dineshappavoo/next.config.js
+++ dineshappavoo/next.config.js
@@ -1,26 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-//isLocalEnv = false;
-let isLocalEnv = process.env.NEXT_LOCAL_GITHUB_PAGES === "true";
-
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isLocalEnv ? "" : "/dineshappavoo.com",
+  output: "export",     // generate /out on build
   trailingSlash: true,
-  //basePath: '/dineshappavoo.com',
-  // For asset references
-  //assetPrefix: '/dineshappavoo.com/',
-  assetPrefix: isLocalEnv ? "" : "/dineshappavoo.com/",
-  output: "export", // ✅ Always export static files
-  trailingSlash: true, // ✅ Ensures paths work correctly for static pages
-  compiler: {
-    styledComponents: true,
-  },
-  //experimental: {
-    //appDir: true, // ✅ Ensures Next.js uses the App Router
-  //},
   images: {
-    unoptimized: true, // This helps with static export
+    unoptimized: true,  // needed for static export
   },
 };
 
