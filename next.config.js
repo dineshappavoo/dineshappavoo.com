d20d198ef6e74ab909b94c6c2d66ed483019df9c
Dinesh Appavoo <dinesha.cit@gmail.com>
2025-12-06 23:15:08 -0800

    Add isDev for local environment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+// Only disable basePath when explicitly running dev mode
+const isDev = process.env.NEXT_LOCAL_GITHUB_PAGES === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: "/dineshappavoo.com",
-  assetPrefix: "/dineshappavoo.com/",
+  basePath: isDev ? "" : "/dineshappavoo.com",
+  assetPrefix: isDev ? "" : "/dineshappavoo.com/",
   trailingSlash: true,
   images: {
     unoptimized: true,
