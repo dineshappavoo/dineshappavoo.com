8b89292de9a60b70bfb5750091cf99e5c64c788f
Dinesh Appavoo <dinesha.cit@gmail.com>
2026-02-09 02:31:20 -0800

    updated home page navigation to DA

--- src/components/home/Navigation.tsx
+++ src/components/home/Navigation.tsx
@@ -43,7 +43,7 @@ export function Navigation() {
               color: 'var(--text-primary)'
             }}
           >
-            Dinesh Appavoo
+            DA
           </Link>
 
           {/* Navigation Links */}
